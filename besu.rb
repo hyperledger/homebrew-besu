9c4998d068515d627ac1f7c55ec5146ef46929ea
Edward Evans <edward.joshua.evans@gmail.com>
2019-12-20 09:35:58 +1000

    release 1.3.8
    Signed-off-by: Edward Evans <edward.joshua.evans@gmail.com>

--- besu.rb
+++ besu.rb
@@ -1,9 +1,9 @@
 class Besu < Formula
   desc "hyperledger besu ethereum client"
   homepage "https://github.com/hyperledger/besu"
-  url "https://dl.bintray.com/hyperledger-org/besu-repo/besu-1.3.7.zip"
+  url "https://dl.bintray.com/hyperledger-org/besu-repo/besu-1.3.8.zip"
   # update with: ./updateBesu.sh <new-version>
-  sha256 "c5d99ab4a6dd1eb98f238af216cb86f079ad988fb2681e25d0fc04cef8745f8c"
+  sha256 "d04a7549998b7a523f1bcf861d47224ff2e4a0e61403361aa66d7c0c72683603"
 
   depends_on :java => "11+"
 
