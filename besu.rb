a20c2b3e3a062755d790981f1375d8e24ed01302
Joshua Fernandes <joshua.fernandes@consensys.net>
2020-05-21 14:19:39 +1000

    updating to 1.4.5
    Signed-off-by: Joshua Fernandes <joshua.fernandes@consensys.net>

--- besu.rb
+++ besu.rb
@@ -1,9 +1,9 @@
 class Besu < Formula
   desc "hyperledger besu ethereum client"
   homepage "https://github.com/hyperledger/besu"
-  url "https://dl.bintray.com/hyperledger-org/besu-repo/besu-1.4.4.zip"
+  url "https://dl.bintray.com/hyperledger-org/besu-repo/besu-1.4.5.zip"
   # update with: ./updateBesu.sh <new-version>
-  sha256 "0f96ff4381e2ad10c3fdec8c4f62599816bed32dec1de1a4abecc20c42127972"
+  sha256 "c543363e5d296bfe839c20b569e347159293b7df628bd261ecafa6fb168be99c"
 
   depends_on :java => "11+"
 
