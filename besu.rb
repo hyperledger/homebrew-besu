5c0ac0f2997f10a3776eb97db0318fd301044a35
Karim TAAM <karim.t2am@gmail.com>
2020-11-03 18:59:21 +0100

    Update to 20.10.0
    Signed-off-by: Karim TAAM <karim.t2am@gmail.com>

--- besu.rb
+++ besu.rb
@@ -1,9 +1,9 @@
 class Besu < Formula
   desc "hyperledger besu ethereum client"
   homepage "https://github.com/hyperledger/besu"
-  url "https://dl.bintray.com/hyperledger-org/besu-repo/besu-1.5.5.zip"
+  url "https://dl.bintray.com/hyperledger-org/besu-repo/besu-20.10.0.zip"
   # update with: ./updateBesu.sh <new-version>
-  sha256 "e67b0a899dc4421054eaa9a8112cb89e1e5f6a56f0d8aa1b0c5111c53dfad2ad"
+  sha256 "2b50a375aae64b838a2cd9d43747006492cae573f1be11745b7f643646fd5a01"
 
   depends_on :java => "11+"
 
