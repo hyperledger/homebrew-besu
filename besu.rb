66e8b3eaf9eeed000f2a89a25086a61a279851f4
Danno Ferrin <danno.ferrin@gmail.com>
2020-12-02 12:04:11 -0700

    20.10.2
    Signed-off-by: Danno Ferrin <danno.ferrin@gmail.com>

--- besu.rb
+++ besu.rb
@@ -1,9 +1,9 @@
 class Besu < Formula
   desc "hyperledger besu ethereum client"
   homepage "https://github.com/hyperledger/besu"
-  url "https://dl.bintray.com/hyperledger-org/besu-repo/besu-20.10.1.zip"
+  url "https://dl.bintray.com/hyperledger-org/besu-repo/besu-20.10.2.zip"
   # update with: ./updateBesu.sh <new-version>
-  sha256 "ac4fae310957c176564396f73c0f03c60c41129d43d078560d0dab533a69fd2a"
+  sha256 "710aed228dcbe9b8103aef39e4431b0c63e73c3a708ce88bcd1ecfa1722ad307"
 
   depends_on :java => "11+"
 
