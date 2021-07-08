632ac8ed10069cf453a6f013f3e51f1917eaf3dc
Ratan Rai Sur <ratan.r.sur@gmail.com>
2021-07-08 13:17:57 -0500

    21.7.0
    Signed-off-by: Ratan Rai Sur <ratan.r.sur@gmail.com>

--- besu.rb
+++ besu.rb
@@ -1,10 +1,9 @@
 class Besu < Formula
   desc "hyperledger besu ethereum client"
   homepage "https://github.com/hyperledger/besu"
-  url "https://hyperledger.jfrog.io/artifactory/besu-binaries/besu/21.1.6/besu-21.1.6.zip"
+  url "https://hyperledger.jfrog.io/artifactory/besu-binaries/besu/21.7.0/besu-21.7.0.zip"
   # update with: ./updateBesu.sh <new-version>
-  sha256 "33b0e15ee9ac5731439b9133bacc12879734cdd0a952032630c1e19bcc114be2"
-
+  sha256 "389465fdcc2cc5e5007a02dc2b8a2c43d577198867316bc5cc4392803ed71034"
 
   depends_on "openjdk" => "11+"
 
