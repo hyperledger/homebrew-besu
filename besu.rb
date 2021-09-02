4fc57d89acada0b66ff0129738304b162c06b855
Justin Florentine <justin.florentine@consensys.net>
2021-09-02 19:17:51 -0400

    updating formula to new release
    Signed-off-by: Justin Florentine <justin.florentine@consensys.net>

--- besu.rb
+++ besu.rb
@@ -1,9 +1,9 @@
 class Besu < Formula
   desc "hyperledger besu ethereum client"
   homepage "https://github.com/hyperledger/besu"
-  url "https://hyperledger.jfrog.io/artifactory/besu-binaries/besu/21.7.2/besu-21.7.2.zip"
+  url "https://hyperledger.jfrog.io/artifactory/besu-binaries/besu/21.7.3/besu-21.7.3.zip"
   # update with: ./updateBesu.sh <new-version>
-  sha256 "db47fd9ba33b36436ed6798d2474f7621c733353fd04f49d6defffd12e3b6e14"
+  sha256 "676c8bbcf4a2e7d08198b0a106a2164e8d55d6f3e1d19783cfd70048be302fb3"
 
   depends_on "openjdk" => "11+"
 
