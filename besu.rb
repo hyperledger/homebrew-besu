9a10808b4d3bae45de91d5ebafbf59d74857d643
Karim TAAM <karim.t2am@gmail.com>
2021-10-12 10:51:56 +0200

    Update to 21.10.0-RC2
    Signed-off-by: Karim TAAM <karim.t2am@gmail.com>

--- besu.rb
+++ besu.rb
@@ -1,9 +1,9 @@
 class Besu < Formula
   desc "hyperledger besu ethereum client"
   homepage "https://github.com/hyperledger/besu"
-  url "https://hyperledger.jfrog.io/artifactory/besu-binaries/besu/21.7.4/besu-21.7.4.zip"
+  url "https://hyperledger.jfrog.io/artifactory/besu-binaries/besu/21.10.0-RC2/besu-21.10.0-RC2.zip"
   # update with: ./updateBesu.sh <new-version>
-  sha256 "778d3c42851db11fec9171f77b22662f2baeb9b2ce913d7cfaaf1042ec19b7f9"
+  sha256 "4a279b515a2525ae42299ba74518a109444776b01c501ee4d96ea345c35194eb"
 
   depends_on "openjdk" => "11+"
 
