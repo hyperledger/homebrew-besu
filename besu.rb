70a24119c3da5a704f74cda5af8f0954538949ff
Danno Ferrin <danno.ferrin@gmail.com>
2021-11-15 13:34:47 -0700

    Release 21.10.2
    Signed-off-by: Danno Ferrin <danno.ferrin@gmail.com>

--- besu.rb
+++ besu.rb
@@ -1,9 +1,9 @@
 class Besu < Formula
   desc "hyperledger besu ethereum client"
   homepage "https://github.com/hyperledger/besu"
-  url "https://hyperledger.jfrog.io/artifactory/besu-binaries/besu/21.10.1/besu-21.10.1.zip"
+  url "https://hyperledger.jfrog.io/artifactory/besu-binaries/besu/21.10.2/besu-21.10.2.zip"
   # update with: ./updateBesu.sh <new-version>
-  sha256 "064df81be49c31ebfc0e1a5f03357e8f6f9a7f5e7bcb82e2b187e2d58e1704eb"
+  sha256 "4b96d4d10c0c6128dc67333c8600e927ea135b4db5e5f74688a0753260b7b985"
 
   depends_on "openjdk" => "11+"
 
