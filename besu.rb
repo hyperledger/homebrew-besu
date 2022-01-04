ca1c4b8e7a47510e9fd9009b7adacdbfcb58fd14
Sally MacFarlane <sally.macfarlane@consensys.net>
2022-01-05 08:57:27 +1000

    Update to 21.10.6 (#66)
    Signed-off-by: Sally MacFarlane <sally.macfarlane@consensys.net>

--- besu.rb
+++ besu.rb
@@ -1,9 +1,9 @@
 class Besu < Formula
   desc "hyperledger besu ethereum client"
   homepage "https://github.com/hyperledger/besu"
-  url "https://hyperledger.jfrog.io/artifactory/besu-binaries/besu/21.10.5/besu-21.10.5.zip"
+  url "https://hyperledger.jfrog.io/artifactory/besu-binaries/besu/21.10.6/besu-21.10.6.zip"
   # update with: ./updateBesu.sh <new-version>
-  sha256 "0deaa924b453a4ff0b6f2886cefaddaccbe5a4bf732d970480fdeae1672b94c2"
+  sha256 "0fdda65bc993905daa14824840724d0b74e3f16f771f5726f5307f6d9575a719"
 
   depends_on "openjdk" => "11+"
 
