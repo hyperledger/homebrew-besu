f87fd226472c7620f8a76547204a9bb787cbf057
Sally MacFarlane <sally.macfarlane@consensys.net>
2022-01-27 14:22:14 +1000

    updated hash since the CI pipeline was re-run on the same commit, generating the same artifact with a different hash
    Signed-off-by: Sally MacFarlane <sally.macfarlane@consensys.net>

--- besu.rb
+++ besu.rb
@@ -3,7 +3,7 @@ class Besu < Formula
   homepage "https://github.com/hyperledger/besu"
   url "https://hyperledger.jfrog.io/artifactory/besu-binaries/besu/21.10.9/besu-21.10.9.zip"
   # update with: ./updateBesu.sh <new-version>
-  sha256 "26489881d852eefdda2cf0203cb30f7b2045ecfb299da8c744e9579cbe2934e2"
+  sha256 "c3ba3f07340fa80064ba7c06f2c0ec081184e000f9a925d132084352d0665ef9"
 
   depends_on "openjdk" => "11+"
 
