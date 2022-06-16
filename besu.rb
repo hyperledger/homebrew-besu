35a68b6d3b91ba4b0d19ce3de0dea1ff0fa87b79
Justin Florentine <justin+github@florentine.us>
2022-06-16 15:01:33 -0400

    22.4.3 (#79)
    * updated formula to latest version (22.4.3)
    
    Signed-off-by: Justin Florentine <justin+github@florentine.us>

--- besu.rb
+++ besu.rb
@@ -1,9 +1,9 @@
 class Besu < Formula
   desc "hyperledger besu ethereum client"
   homepage "https://github.com/hyperledger/besu"
-  url "https://hyperledger.jfrog.io/artifactory/besu-binaries/besu/22.4.2/besu-22.4.2.zip"
+  url "https://hyperledger.jfrog.io/artifactory/besu-binaries/besu/22.4.3/besu-22.4.3.zip"
   # update with: ./updateBesu.sh <new-version>
-  sha256 "e8e9eb7e3f544ecefeec863712fb8d3f6a569c9d70825a4ed2581c596db8fd45"
+  sha256 "144f4c407193cb66897ac5999b215507333df6b418c84d607bd68e16a179ab64"
 
   depends_on "openjdk" => "11+"
 
