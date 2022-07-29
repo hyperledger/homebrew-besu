a90b5c3086a0ab0a17fb7f50e5449eecf33af470
garyschulte <garyschulte@gmail.com>
2022-07-29 07:40:34 -0700

    releasing 22.7.0-RC3 due to known defects with 22.4.4 latest release version (#81)
    Signed-off-by: garyschulte <garyschulte@gmail.com>

--- besu.rb
+++ besu.rb
@@ -1,9 +1,9 @@
 class Besu < Formula
   desc "hyperledger besu ethereum client"
   homepage "https://github.com/hyperledger/besu"
-  url "https://hyperledger.jfrog.io/artifactory/besu-binaries/besu/22.4.4/besu-22.4.4.zip"
+  url "https://hyperledger.jfrog.io/artifactory/besu-binaries/besu/22.7.0-RC3/besu-22.7.0-RC3.zip"
   # update with: ./updateBesu.sh <new-version>
-  sha256 "84a7bee8fc35c78fd6d9e7bbdc5cd28c577d95487480bb03b9642a3111b71bc3"
+  sha256 "5de22445ab2a270cf33e1850cd28f1946442b7104738f0d1ac253a009c53414e"
 
   depends_on "openjdk" => "11+"
 
