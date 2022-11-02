93aa3745fb67314fcd6323e2c63b6ebd7837f6d8
Fabio Di Fabio <fabio.difabio@consensys.net>
2022-11-02 19:56:00 +0100

    Release 22.10.0 (#90)
    Signed-off-by: Fabio Di Fabio <fabio.difabio@consensys.net>

--- besu.rb
+++ besu.rb
@@ -1,9 +1,9 @@
 class Besu < Formula
   desc "hyperledger besu ethereum client"
   homepage "https://github.com/hyperledger/besu"
-  url "https://hyperledger.jfrog.io/artifactory/besu-binaries/besu/22.7.7/besu-22.7.7.zip"
+  url "https://hyperledger.jfrog.io/hyperledger/besu-binaries/besu/22.10.0/besu-22.10.0.zip"
   # update with: ./updateBesu.sh <new-version>
-  sha256 "79b2b1518605603d8268f873f2576617ca8340d89c045e0eda6896f40defea0d"
+  sha256 "8ad4927469e8e128a3a2c7a708f108393eebc82c522f66cdcac4b7d206e07f90"
 
   depends_on "openjdk" => "11+"
 
