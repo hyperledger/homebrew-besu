f1d202fff04c22a8d4c66c14c288d36fd18ac4e0
garyschulte <garyschulte@gmail.com>
2022-12-01 15:12:38 -0800

    release 22.10.1 (#92)
    Signed-off-by: garyschulte <garyschulte@gmail.com>

--- besu.rb
+++ besu.rb
@@ -1,9 +1,9 @@
 class Besu < Formula
   desc "hyperledger besu ethereum client"
   homepage "https://github.com/hyperledger/besu"
-  url "https://hyperledger.jfrog.io/artifactory/besu-binaries/besu/22.10.0/besu-22.10.0.zip"
+  url "https://hyperledger.jfrog.io/artifactory/besu-binaries/besu/22.10.1/besu-22.10.1.zip"
   # update with: ./updateBesu.sh <new-version>
-  sha256 "c8e39f7c879409cb9b47f4d3de5e9c521249083830a8c9a45e8a14a319fe195d"
+  sha256 "0dbee534620c7cc0fac0596e6df0c7f8a74be9df9cecd9d4f1407016f30fb9a1"
 
   depends_on "openjdk" => "11+"
 
