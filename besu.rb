8ac763e95a755557d7e5b002cee22d87e0d50639
Jason Frame <jason.frame@consensys.net>
2022-12-15 10:17:03 +1000

    release 22.10.3 (#94)
    Signed-off-by: Jason Frame <jason.frame@consensys.net>

--- besu.rb
+++ besu.rb
@@ -1,9 +1,9 @@
 class Besu < Formula
   desc "hyperledger besu ethereum client"
   homepage "https://github.com/hyperledger/besu"
-  url "https://hyperledger.jfrog.io/artifactory/besu-binaries/besu/22.10.2/besu-22.10.2.zip"
+  url "https://hyperledger.jfrog.io/artifactory/besu-binaries/besu/22.10.3/besu-22.10.3.zip"
   # update with: ./updateBesu.sh <new-version>
-  sha256 "4c9208f684762670cb4f2c6ebfb6930e05e339a7c3c586fe8caa9f26462830aa"
+  sha256 "0bf6bc98e01b0c1045f1b7d841a390c575bc5203c2a4e543d922fbc1ea0d3d5d"
 
   depends_on "openjdk" => "11+"
 
