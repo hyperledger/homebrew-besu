f5ac228578df5e03eee515bb29fa9f02fd41a5cc
garyschulte <garyschulte@gmail.com>
2023-08-21 16:08:02 -0700

    release for 23.7.1 (#102)
    Signed-off-by: garyschulte <garyschulte@gmail.com>

--- besu.rb
+++ besu.rb
@@ -1,9 +1,9 @@
 class Besu < Formula
   desc "hyperledger besu ethereum client"
   homepage "https://github.com/hyperledger/besu"
-  url "https://hyperledger.jfrog.io/artifactory/besu-binaries/besu/23.4.4/besu-23.4.4.zip"
+  url "https://hyperledger.jfrog.io/artifactory/besu-binaries/besu/23.7.1/besu-23.7.1.zip"
   # update with: ./updateBesu.sh <new-version>
-  sha256 "4575000f4fd21d318e7b77340c9281d496bc800bee5b45a13684319e6f28bf27"
+  sha256 "dfac11b2d6d9e8076ab2f86324d48d563badf76fd2a4aadc4469a97aef374ef5"
 
   depends_on "openjdk" => "11+"
 
