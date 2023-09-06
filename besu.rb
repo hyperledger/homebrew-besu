6878b59a8f350cf6c1c9404f669df8326abe6537
garyschulte <garyschulte@gmail.com>
2023-09-06 10:45:40 -0700

    update homebrew to 23.7.2
    Signed-off-by: garyschulte <garyschulte@gmail.com>

--- besu.rb
+++ besu.rb
@@ -1,9 +1,9 @@
 class Besu < Formula
   desc "hyperledger besu ethereum client"
   homepage "https://github.com/hyperledger/besu"
-  url "https://hyperledger.jfrog.io/artifactory/besu-binaries/besu/23.1.1/besu-23.1.1.zip"
+  url "https://hyperledger.jfrog.io/artifactory/besu-binaries/besu/23.7.2/besu-23.7.2.zip"
   # update with: ./updateBesu.sh <new-version>
-  sha256 "afcf852f193adb8e82d187aa4f02e4669f12cc680270624d37101b94cf37adec"
+  sha256 "a233c83591fc277e3d1530c84bb5ea896abad717d796b5e3b856c79199132b75"
 
   depends_on "openjdk" => "11+"
 
