afb724fca6695bcae793db7c2487c6bbf52830aa
Danno Ferrin <danno.ferrin@gmail.com>
2023-10-12 13:48:48 -0600

    Release 23.10.0
    Signed-off-by: Danno Ferrin <danno.ferrin@gmail.com>

--- besu.rb
+++ besu.rb
@@ -1,9 +1,9 @@
 class Besu < Formula
   desc "hyperledger besu ethereum client"
   homepage "https://github.com/hyperledger/besu"
-  url "https://hyperledger.jfrog.io/artifactory/besu-binaries/besu/23.7.3/besu-23.7.3.zip"
+  url "https://hyperledger.jfrog.io/artifactory/besu-binaries/besu/23.10.0/besu-23.10.0.zip"
   # update with: ./updateBesu.sh <new-version>
-  sha256 "136596454f647c706130e3e2983bdbb4a1cbfaf2bbf6e999466754f9213c11f6"
+  sha256 "d5dafff4c3cbf104bf75b34a9f108dcdd7b08d2759de75ec65cd997f38f52866"
 
   depends_on "openjdk" => "17+"
 
