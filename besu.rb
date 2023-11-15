e800d423a57ceb63417fb7eab9b21ac0ed0f94ac
Simon Dudley <simon.dudley@consensys.net>
2023-11-15 19:57:26 +1000

    Update SHA since the CI build accidentally reran and overwrote it Signed-off-by: Simon Dudley <simon.dudley@consensys.net>

--- besu.rb
+++ besu.rb
@@ -3,7 +3,7 @@ class Besu < Formula
   homepage "https://github.com/hyperledger/besu"
   url "https://hyperledger.jfrog.io/artifactory/besu-binaries/besu/23.10.1/besu-23.10.1.zip"
   # update with: ./updateBesu.sh <new-version>
-  sha256 "fb173acb93c72fbb74a6542051691ca2d3d5f54ea2f51026467a512f3a22106b"
+  sha256 "6a591bb4ae6f3df7371fee7676dcd1d34925c5c2e7790cc783037e4b0d8b6292"
 
   depends_on "openjdk" => "17+"
 
