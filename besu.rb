7052bfcbd1cd466744041d282ce0edb7e3beb415
Daniel Varela <dotexe0@users.noreply.github.com>
2024-02-14 14:27:55 -0800

    Release 24.1.2
    Signed-off-by: Daniel Varela <dotexe0@users.noreply.github.com>

--- besu.rb
+++ besu.rb
@@ -1,9 +1,9 @@
 class Besu < Formula
   desc "hyperledger besu ethereum client"
   homepage "https://github.com/hyperledger/besu"
-  url "https://hyperledger.jfrog.io/artifactory/besu-binaries/besu/24.1.1/besu-24.1.1.zip"
+  url "https://hyperledger.jfrog.io/artifactory/besu-binaries/besu/24.1.2/besu-24.1.2.zip"
   # update with: ./updateBesu.sh <new-version>
-  sha256 "e23c5b790180756964a70dcdd575ee2ed2c2efa79af00bce956d23bd2f7dc67c"
+  sha256 "9033f300edd81c770d3aff27a29f59dd4b6142a113936886a8f170718e412971"
 
   depends_on "openjdk" => "17+"
 
