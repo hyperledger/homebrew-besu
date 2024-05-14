54eb401fb9ad08da73759b7746a3dc7ef180ee16
Sally MacFarlane <macfarla.github@gmail.com>
2024-05-14 10:34:30 +1000

    24.5.1
    Signed-off-by: Sally MacFarlane <macfarla.github@gmail.com>

--- besu.rb
+++ besu.rb
@@ -1,9 +1,9 @@
 class Besu < Formula
   desc "hyperledger besu ethereum client"
   homepage "https://github.com/hyperledger/besu"
-  url "https://github.com/hyperledger/besu/releases/download/24.3.3/besu-24.3.3.zip"
+  url "https://github.com/hyperledger/besu/releases/download/24.5.1/besu-24.5.1.zip"
   # update with: ./updateBesu.sh <new-version>
-  sha256 "b44951e68c6978de7f289ebd5f0111f0087cd266fd0c133afcf33b2004aa1a2a"
+  sha256 "13d75b6b22e1303f39fd3eaddf736b24ca150b2bafa7b98fce7c7782e54b213f"
 
   depends_on "openjdk" => "17+"
 
