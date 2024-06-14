4ba423d8a149d76b7370aa3d2e2d649748817f99
Jason Frame <jason.frame@consensys.net>
2024-06-14 11:20:09 +1000

    release 24.6.0
    Signed-off-by: Jason Frame <jason.frame@consensys.net>

--- besu.rb
+++ besu.rb
@@ -1,11 +1,11 @@
 class Besu < Formula
   desc "hyperledger besu ethereum client"
   homepage "https://github.com/hyperledger/besu"
-  url "https://github.com/hyperledger/besu/releases/download/24.5.2/besu-24.5.2.zip"
+  url "https://github.com/hyperledger/besu/releases/download/24.6.0/besu-24.6.0.zip"
   # update with: ./updateBesu.sh <new-version>
-  sha256 "23966b501a69e320e8f8f46a3d103ccca45b53f8fee35a6543bd9a260b5784ee"
+  sha256 "8b2d3a674cd7ead68b9ca68fea21e46d5ec9b278bbadc73f8c13c6a1e1bc0e4d"
 
-  depends_on "openjdk" => "17+"
+  depends_on "openjdk" => "21+"
 
   def install
     prefix.install "lib"
